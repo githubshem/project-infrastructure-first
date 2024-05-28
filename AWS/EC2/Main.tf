07387269def913b3127373692ca2621f9a5c773a
githubshem <shemsumbelingforwork@gmail.com>
2024-05-28 19:42:11 +0800

    second-commit

--- AWS/EC2/Main.tf
+++ AWS/EC2/Main.tf
@@ -242,7 +242,7 @@ resource "aws_launch_template" "lt_east_1" {
 # Security Group
 ####################################################
 
-resource "aws_security_group" "sg_2" {
+/* resource "aws_security_group" "sg_2" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.main.id
@@ -264,4 +264,4 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
-}
+} */
--- AWS/IAM/Main.tf
+++ AWS/IAM/Main.tf
@@ -18,24 +18,95 @@ provider "aws" {
 }
 
 ####################################################
-# EC2 Instance
+# User group
 ####################################################
 
+resource "aws_iam_group" "administrator_1" {
+  name = "administrator_1"
+  path = "/users/"
+}
 
-
-
-
-
-####################################################
-# EC2 Instance
-####################################################
-
-
-
-
-
-
+resource "aws_iam_group" "developers_1" {
+  name = "developer_1"
+  path = "/users/"
+}
 
 ####################################################
-# EC2 Instance
+# Users
 ####################################################
+
+resource "aws_iam_user" "user_1" {
+  name = "ec2"
+  path = "/system/"
+}
+
+/* resource "aws_iam_access_key" "lb" {
+  user = aws_iam_user.lb.name
+}
+
+data "aws_iam_policy_document" "lb_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name   = "test"
+  user   = aws_iam_user.lb.name
+  policy = data.aws_iam_policy_document.lb_ro.json
+} */
+
+####################################################
+# Roles
+####################################################
+
+/* resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+} */
+
+####################################################
+# Policy
+####################################################
+
+/* resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+} */
